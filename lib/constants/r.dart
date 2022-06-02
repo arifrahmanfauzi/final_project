9202fea9f134e021903536113f8555257fc084a1
arif <arif.rahman1780@gmail.com>
2022-06-02 12:09:56 +0700

    initial commit

---
+++ lib/constants/r.dart
@@ -0,0 +1,9 @@
+import 'package:final_project/constants/R/app_colors.dart';
+import 'package:final_project/constants/R/assets.dart';
+import 'package:final_project/constants/R/strings.dart';
+
+class R {
+  static AppColor colors = AppColor();
+  static Assets assets = Assets();
+  static Strings strings = Strings();
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:final_project/constants/r.dart';
+import 'package:final_project/view/login_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  static const String route = "splash_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    //Create Timer splashscreen
+    Timer(const Duration(seconds: 3), () {
+      //Push navigator normal
+      //   Navigator.of(context)
+      //       .push(MaterialPageRoute(builder: (context) => LoginPage()));
+
+      //push navigator with named routing
+      Navigator.of(context).pushNamed(LoginPage.route);
+    });
+
+    return Scaffold(
+      backgroundColor: R.colors.primary,
+      body: Center(
+        child: Image.asset(R.assets.icSplash),
+      ),
+    );
+  }
+}
